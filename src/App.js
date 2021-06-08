b38005dd96b804db381c81be09013ca12979f636
berbiche <berbiche@gmail.com>
2021-06-08 11:58:03 +0200

    nettoyage

--- src/App.js
+++ src/App.js
@@ -2,13 +2,10 @@ import "./App.css"; // import de styles globaux
 import Header from "./Header/Header";
 
 function App() {
-  const texte = "Premières petites manips";
   const conatainerClassName = "App";
   return (
     <div className={conatainerClassName}>
       <Header></Header>
-      <h2>Hello CDA 2021-1</h2>
-      <p>{texte.toUpperCase()}</p>
     </div>
   );
 }
